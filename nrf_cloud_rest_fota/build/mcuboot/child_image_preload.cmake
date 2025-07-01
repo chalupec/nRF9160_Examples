18c817500c9a88a5ffdf96efc90780c6fa4d4790
JCH <chalupa@adwitech.cz>
2025-07-02 01:48:44 +0200

    proste setup red reseni ADC + modem

---
+++ nrf_cloud_rest_fota/build/mcuboot/child_image_preload.cmake
@@ -0,0 +1,10 @@
+# Generated file used for preloading a child image
+set(CMAKE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(CMAKE_VERBOSE_MAKEFILE "FALSE" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_DIR "C:/ncs/v2.6.0/zephyr/boards/arm/nrf9161dk_nrf9161" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_REVISION "0.9.0" CACHE INTERNAL "NCS child image controlled")
+set(ZEPHYR_TOOLCHAIN_VARIANT "zephyr" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
+set(BOARD "nrf9161dk_nrf9161" CACHE INTERNAL "NCS child image controlled")
+set(OVERLAY_CONFIG "C:/ncs/v2.6.0/nrf/subsys/bootloader/image/multi_image_mcuboot.conf;C:/ncs/v2.6.0/nrf/subsys/bootloader/image/fw_info.conf;C:/ncs/v2.6.0/nrf/subsys/bootloader/image/build_s1.conf;C:/ncs/v2.6.0/nrf/modules/mcuboot/tfm.conf;C:/ncs/v2.6.0/nrf/modules/mcuboot/fih_low_enable.conf;C:/ncs/v2.6.0/nrf/subsys/partition_manager/partition_manager_enabled.conf;C:/ADWITECH/nRF9160_Examples/nrf_cloud_rest_fota/child_image/mcuboot.conf" CACHE INTERNAL "NCS child image controlled")
